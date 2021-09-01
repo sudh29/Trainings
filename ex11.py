ec0acdc9d9c0f5f209c07d76c9e38ea83d85ee39
sudh29 <73557822+sudh29@users.noreply.github.com>
2021-09-01 18:36:57 +0530

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,18 @@
+# Function to find out ways to form a string from another string.
+# Function will take 2 strings as inputs. You must find out all 
+# possible ways by which you can form string A from String B. You 
+# must validate to ensure that the strings are Alpha-numeric only, 
+# it must contain only strings and numbers. No other types are 
+# allowed. The length of String B must be >er than String A.
+# Char once used from String B must not be used again.
+# Return the final Output in dictionary format with indexes and values.
+# Indexes represent the index value in String B that has occurrence of 
+# a char from String A. Function to find out ways to form a string from 
+# another string. Function will take 2 strings as inputs.
+
+def ex11():
+    return
+
+
+# Run the function for given input
+print(ex11("abc", "agcb xyzbc amnopq copnotab coscab"))
