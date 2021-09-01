ccb4e8d472ba22aaf543c3b9234afbea28b9a85d
sudh29 <73557822+sudh29@users.noreply.github.com>
2021-09-01 20:05:48 +0530

    Add files via upload

--- ex11.py
+++ ex11.py
@@ -10,19 +10,51 @@
 # a char from String A. Function to find out ways to form a string from 
 # another string. Function will take 2 strings as inputs.
 
+
+# function for length of array
+def lenfn(arr):
+    c=0
+    for i in arr:
+        c+=1
+    return c
+
+# find min char and slice others
+def index_mins(index):
+    mintemp=float('inf')
+    for i in index:
+        mintemp=min(mintemp,len(i))
+    temp=[]
+    for i in index:
+        temp.append(i[0:mintemp])
+    return temp
+
+
+# dictionary sorting function based on keys
+def sortdic(temp):
+    temp2 = {}
+    for j in sorted(temp):
+        temp2[j] = temp[j]
+    return temp2
+
+
+# take two string as input
 def ex11(list_str1,list_str2):
+    index=[]
+    for i in list_str1:
+        temp=[idx for idx, val in enumerate(list_str2) if val==i]
+        index.append(temp)
+    sliced_idx=index_mins(index)
     op=[]
     res={}
-    for idx, val in enumerate(list_str2):
-        if val in list_str1:
-            res[idx]=val
-        if len(res)==len(list_str1):
-            op.append(res)
-            res={}
+    for i in range(lenfn(sliced_idx[0])):
+        for j in range(lenfn(sliced_idx)):
+            res[sliced_idx[j][i]]=list_str1[j]
+        op.append(sortdic(res))
+        res={}
     return op
 
 
 # Run the function for given input
-print(ex11("abc", "agcb xyzbc amnopq copnotab coscab"))
+print(ex11("abc", "agcb xyzbc amnopq copnotab cosxab"))
 
-# print(ex11("ab", "agcb xyzbc amnopq copnotab coscabbb"))
+print(ex11("ab", "agcb xyzbc amnopq copnotab coscabbb"))
